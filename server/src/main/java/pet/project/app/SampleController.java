046cb3df0b5ec9809e613531db481694f3d33e62
elena <hellnellt@gmail.com>
2019-08-17 15:34:52 +0200

    Mock endpoint for categories and questions

--- server/src/main/java/pet/project/app/SampleController.java
+++ server/src/main/java/pet/project/app/SampleController.java
@@ -1,14 +1,31 @@
 package pet.project.app;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 public class SampleController {
 
-    @RequestMapping(path = "/", method = RequestMethod.GET)
-    public void doNothing() {
+    @GetMapping(path = "/categories")
+    public List<String> getCategories() {
+        return Arrays.asList("Java Core", "Collections", "Multithreading");
+    }
 
+    @GetMapping(path = "/questions/{categoryId}")
+    public List<String> getQuestions(@PathParam(value = "categoryId") String categoryId) {
+        switch (categoryId) {
+            case "0":
+                return Arrays.asList("Java Core 1", "Java Core 2");
+            case "1":
+                return Arrays.asList("Collections 1", "Collections 2");
+            case "2":
+                return Arrays.asList("Multithreading 1", "Multithreading 2");
+        }
+        return Collections.emptyList();
     }
 }
